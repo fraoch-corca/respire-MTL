5ccb28202d3bce924aa3fb8af143bbb25d5a0e4c
Cathal <fraoch_corca@protonmail.com>
2026-01-14 00:08:54 -0500

    Adds runtime check on weather API response, fixes Weather interface type

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -17,15 +17,41 @@ interface Main {
 }
 
 interface Conditions {
-    weather: Weather;
+    weather: Weather[];
     wind: Wind;
     main: Main;
 }
 
 const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?lat=45.5088&lon=-73.5878&appid=b98e39bbfd36a8436269e3a3a112e989";
+const WEATHER_PROPERTIES = ['main', 'description'];
+const WIND_PROPERTIES = ['gust', 'deg', 'speed'];
+const MAIN_PROPERTIES = ['temp', 'humidity'];
+
+function validateData (data: Conditions) {
+    if (!data.weather || !Array.isArray(data.weather) || !data.weather.length) {
+        throw new Error('data Weather does not conform to Conditions interface');
+    }
+
+    WEATHER_PROPERTIES.forEach((property) => {
+        if (!data.weather[0].hasOwnProperty(property)) {
+            throw new Error(`Expected Weather ${property} for Conditions interface`);
+        }
+    });
+
+    WIND_PROPERTIES.forEach((property) => {
+        if (!data.wind.hasOwnProperty(property)) {
+            throw new Error(`Expected Wind ${property} for Conditions interface`);
+        }
+    });
+
+    MAIN_PROPERTIES.forEach((property) => {
+        if (!data.main.hasOwnProperty(property)) {
+            throw new Error(`Expected Main ${property} for Conditions interface`);
+        }
+    });
+}
 
 async function fetchWeatherData(): Promise<Conditions> {
-    
     const response = await fetch(WEATHER_API);
 
     if (!response.ok) {
@@ -34,9 +60,24 @@ async function fetchWeatherData(): Promise<Conditions> {
 
     const data: Conditions = await response.json();
 
-    console.log('conditions ', data);
+    validateData(data);
 
-    return data;
+    // Extract required properties, return as per Conditions interface
+    return {
+        weather: data.weather.map((w: Weather) => ({
+            main: w.main,
+            description: w.description,
+        })),
+        wind: {
+            speed: data.wind.speed,
+            deg: data.wind.deg,
+            gust: data.wind.gust,
+        },
+        main: {
+            temp: data.main.temp,
+            humidity: data.main.humidity,
+        },
+    };
 }
 
 export default async function WeatherPage() {
