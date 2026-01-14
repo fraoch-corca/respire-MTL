ba671efdf9df75c0c60a211668ca4120255bb57b
Cathal <fraoch_corca@protonmail.com>
2026-01-14 02:55:28 -0500

    Adds runtime check on fetched AQI data

--- src/app/aqi/page.tsx
+++ src/app/aqi/page.tsx
@@ -8,13 +8,25 @@ interface ListItem {
   main: Main;
 }
 
-interface AirQualityResponse {
+interface AirQuality {
   list: ListItem[]; 
 }
 
 const AQI_API = "http://api.openweathermap.org/data/2.5/air_pollution?lat=45.5088&lon=-73.5878&appid=b98e39bbfd36a8436269e3a3a112e989";
 
 const aqiCache = new NodeCache();
+
+function validateData (data: AirQuality) {
+    const error = "Data does not conform to AirQuality interface";
+
+    if (!data.list || !Array.isArray(data.list) || !data.list.length) {
+        throw new Error(error);
+    }
+
+    if (!data.list[0].main?.aqi) {
+      throw new Error(error);
+    }
+}
 
 async function fetchAirQualityData(): Promise<number> {
     const cachedData = aqiCache.get<number>("aqiValue");
@@ -30,12 +42,14 @@ async function fetchAirQualityData(): Promise<number> {
         throw new Error('AQI data fetch fail');
     }
 
-    const data: AirQualityResponse = await response.json();
+    const data: AirQuality = await response.json();
 
-    const aqi = data.list[0]?.main.aqi;
+    validateData(data);
+
+    const aqi = data.list[0].main.aqi;
 
     aqiCache.set("aqiValue", aqi, 3600);
-    
+
     return aqi;
 }
 
